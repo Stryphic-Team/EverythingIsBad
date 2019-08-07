7388b70b4b5818f5b4a57c345c4c3d1c7729d59b
anakrusis <howtofamitracker@gmail.com>
2019-08-07 17:22:43 -0400

    Poop texture

--- src/main/java/com/dna/everythingisbad/init/ModItems.java
+++ src/main/java/com/dna/everythingisbad/init/ModItems.java
@@ -2,7 +2,11 @@ package com.dna.everythingisbad.init;
 
 
 import com.dna.everythingisbad.item.ItemPoop;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,5 +15,14 @@ public class ModItems {
 
     public static final List<Item> ITEMS = new ArrayList<Item>();
     public static final Item POOP_ITEM = new ItemPoop("poop");
+
+    @SubscribeEvent
+    public static void registerRenders(ModelRegistryEvent event) {
+        registerRender(POOP_ITEM);
+    }
+
+    private static void registerRender(Item item) {
+        ModelLoader.setCustomModelResourceLocation(item, 3, new ModelResourceLocation( item.getRegistryName(), "inventory"));
+    }
 
 }
