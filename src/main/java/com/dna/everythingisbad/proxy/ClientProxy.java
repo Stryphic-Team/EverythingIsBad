2678d825c61044aeeaa4f34ef3d6bd6101184358
anakrusis <howtofamitracker@gmail.com>
2019-08-08 01:53:47 -0400

    Poop key listener fixed!

--- src/main/java/com/dna/everythingisbad/proxy/ClientProxy.java
+++ src/main/java/com/dna/everythingisbad/proxy/ClientProxy.java
@@ -1,6 +1,8 @@
 package com.dna.everythingisbad.proxy;
 
+import com.dna.everythingisbad.Main;
 import com.dna.everythingisbad.init.ModEntities;
+import com.dna.everythingisbad.utils.handlers.KeyHandler;
 import com.dna.everythingisbad.utils.handlers.TestHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
@@ -8,6 +10,7 @@ import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
 public class ClientProxy extends CommonProxy{
     public void registerItemRenderer(Item item, int meta, String id)
@@ -23,6 +26,11 @@ public class ClientProxy extends CommonProxy{
 
         registerEntityRenderer();
 
+    }
+    @Override
+    public void postInit(FMLPostInitializationEvent event) {
+
+        MinecraftForge.EVENT_BUS.register(new KeyHandler());
     }
 
 }
--- src/main/java/com/dna/everythingisbad/utils/handlers/KeyHandler.java
+++ src/main/java/com/dna/everythingisbad/utils/handlers/KeyHandler.java
@@ -2,6 +2,7 @@ package com.dna.everythingisbad.utils.handlers;
 
 import com.dna.everythingisbad.Main;
 import com.dna.everythingisbad.reference.Reference;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.common.MinecraftForge;
@@ -11,6 +12,7 @@ import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
 import org.lwjgl.input.Keyboard;
 
 import java.util.ArrayList;
@@ -19,6 +21,9 @@ import static java.lang.System.out;
 
 @Mod.EventBusSubscriber
 public class KeyHandler {
+
+    static final Minecraft mc = Minecraft.getMinecraft();
+
     @SubscribeEvent
     public void pickupItem(EntityItemPickupEvent event) {
         Main.logger.info("Item picked up!");
@@ -26,14 +31,34 @@ public class KeyHandler {
 
     private static KeyBinding poopkey;
 
+    private static ArrayList<KeyBinding> keys = new ArrayList<KeyBinding>();
+
     public KeyHandler(){
         poopkey = new KeyBinding(Reference.PREFIX + "keybind.poopkey", Keyboard.KEY_P, Reference.PREFIX + "category." + Reference.MOD_ID);
         ClientRegistry.registerKeyBinding(poopkey);
+        addKeys();
     }
 
     @SubscribeEvent
-    public void onKeyInput(InputEvent.KeyInputEvent event){
-        EntityPlayer player = FMLClientHandler.instance().getClient().player;
+    public void onKeyInput(KeyInputEvent event){
 
+        EntityPlayer player = FMLClientHandler.instance().getClient().player;
+        for (int i=0;i<keys.size();i++) {
+            KeyBinding keyBindings = keys.get(i);
+            //Main.logger.info("Going thru keys");
+            int button = keyBindings.getKeyCode();
+            if ( keyBindings.isPressed()) {
+
+                //Main.logger.info(keyBindings.getKeyDescription());
+
+                if (keyBindings.getKeyDescription().equals(Reference.PREFIX + "keybind.poopkey")) {
+                    Main.logger.info("Poop key pressed!");
+                }
+            }
+        }
+    }
+
+    public static void addKeys() {
+        keys.add(poopkey);
     }
 }
