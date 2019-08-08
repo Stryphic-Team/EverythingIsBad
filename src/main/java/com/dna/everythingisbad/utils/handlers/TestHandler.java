8e40bce68aa906199db92094cd973e9cc3eccc48
DeaSTL <jhartway99@gmail.com>
2019-08-08 00:37:43 -0400

    Removed bot spam

--- src/main/java/com/dna/everythingisbad/utils/handlers/TestHandler.java
+++ src/main/java/com/dna/everythingisbad/utils/handlers/TestHandler.java
@@ -2,13 +2,27 @@ package com.dna.everythingisbad.utils.handlers;
 
 import com.dna.everythingisbad.Main;
 
+import com.dna.everythingisbad.init.ModItems;
+import net.minecraft.client.Minecraft;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.relauncher.Side;
+
 
 
 public class TestHandler {
-    @SubscribeEvent
-    public void timer(TickEvent event){
-        Main.logger.info("A tick happened");
+    int tick_count = 0;
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void timer(TickEvent.ClientTickEvent event){
+        tick_count++;
+        if(tick_count > 100){
+//            ItemStack stack = new ItemStack(ModItems.POOP_ITEM);
+//            Item item = stack.getItem();
+//            Minecraft.getMinecraft().player.inventory.addItemStackToInventory(stack);
+        }
     }
 }
