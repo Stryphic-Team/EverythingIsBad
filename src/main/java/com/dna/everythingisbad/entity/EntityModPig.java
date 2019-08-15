17674e3c1d5e5e1a73a4177e31a450a263cf4099
anakrusis <howtofamitracker@gmail.com>
2019-08-15 00:50:20 -0400

    Dumb test entity

---
+++ src/main/java/com/dna/everythingisbad/entity/EntityModPig.java
@@ -0,0 +1,10 @@
+package com.dna.everythingisbad.entity;
+
+import net.minecraft.entity.passive.EntityPig;
+import net.minecraft.world.World;
+
+public class EntityModPig extends EntityPig {
+    public EntityModPig(World worldIn) {
+        super(worldIn);
+    }
+}
