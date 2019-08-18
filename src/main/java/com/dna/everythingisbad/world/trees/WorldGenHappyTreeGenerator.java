d32c01d4c0984412dcdf9e2274e6b97e374bc33a
DeaSTL <jhartway99@gmail.com>
2019-08-17 21:07:02 -0400

    Forgot to take a logger out of the code

--- src/main/java/com/dna/everythingisbad/world/trees/WorldGenHappyTreeGenerator.java
+++ src/main/java/com/dna/everythingisbad/world/trees/WorldGenHappyTreeGenerator.java
@@ -1,6 +1,5 @@
 package com.dna.everythingisbad.world.trees;
 
-import com.dna.everythingisbad.Main;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
@@ -21,7 +20,7 @@ public class WorldGenHappyTreeGenerator implements IWorldGenerator {
         WorldGenTreeHappy tree = new WorldGenTreeHappy();
         int height_max = 100;
         int height_min = 60;
-        Main.logger.info(random.nextInt(height_max)-height_min);
+
         tree.generate(world,random,new BlockPos(xPos,random.nextInt(height_max + height_min)-height_min,zPos));
 
     }
