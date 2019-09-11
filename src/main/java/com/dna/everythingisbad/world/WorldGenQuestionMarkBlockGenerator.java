a7c6042045cc543e9c0b5516fa224ead51fe9fcf
anakrusis <howtofamitracker@gmail.com>
2019-09-11 00:42:43 -0400

    No more question mark blocks in heaven or nether or end(?)
    Signed-off-by: anakrusis <howtofamitracker@gmail.com>

--- src/main/java/com/dna/everythingisbad/world/WorldGenQuestionMarkBlockGenerator.java
+++ src/main/java/com/dna/everythingisbad/world/WorldGenQuestionMarkBlockGenerator.java
@@ -1,9 +1,12 @@
 package com.dna.everythingisbad.world;
 
+import com.dna.everythingisbad.init.ModBiomes;
 import com.dna.everythingisbad.world.trees.WorldGenHappyTreeGenerator;
 import com.dna.everythingisbad.world.trees.WorldGenTreeHappy;
+import net.minecraft.init.Biomes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraftforge.fml.common.IWorldGenerator;
@@ -20,9 +23,12 @@ public class WorldGenQuestionMarkBlockGenerator implements IWorldGenerator {
         if (random.nextFloat() < 0.1) {
             int xPos = chunkX * 16 + 8;
             int zPos = chunkZ * 16 + 8;
-            WorldGenQuestionMarkBlock blok = new WorldGenQuestionMarkBlock();
-            BlockPos top = world.getTopSolidOrLiquidBlock(new BlockPos(xPos, 1, zPos));
-            blok.generate(world, random, new BlockPos(xPos, top.getY(), zPos));
+            Biome bingly = world.getBiomeProvider().getBiome(new BlockPos(xPos,1,zPos));
+            if (bingly != Biomes.HELL && bingly != ModBiomes.HEAVEN && bingly != Biomes.VOID){
+                WorldGenQuestionMarkBlock blok = new WorldGenQuestionMarkBlock();
+                BlockPos top = world.getTopSolidOrLiquidBlock(new BlockPos(xPos, 1, zPos));
+                blok.generate(world, random, new BlockPos(xPos, top.getY(), zPos));
+            }
         }
     }
 }
