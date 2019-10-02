c6ce6da34321b313c1221e8be20bb4438972e9b6
DeaSTL <jhartway99@gmail.com>
2019-10-02 14:17:15 -0400

    Updated project to 0.1.1-alpha

--- src/main/java/com/dna/everythingisbad/reference/Reference.java
+++ src/main/java/com/dna/everythingisbad/reference/Reference.java
@@ -3,7 +3,7 @@ package com.dna.everythingisbad.reference;
 public class Reference {
     public static final String MOD_ID = "everythingbad";
     public static final String MOD_NAME = "Everything Is Bad";
-    public static final String VERSION = "1.12.2-0.1.0-alpha";
+    public static final String VERSION = "1.12.2-0.1.1-alpha";
 
     public static final String CLIENT_PROXY_CLASS = "com.dna.everythingisbad.proxy.ClientProxy";
     public static final String COMMON_PROXY_CLASS = "com.dna.everythingisbad.proxy.CommonProxy";
--- src/main/java/com/dna/everythingisbad/world/StructureGenerator.java
+++ src/main/java/com/dna/everythingisbad/world/StructureGenerator.java
@@ -4,7 +4,6 @@ import com.dna.everythingisbad.init.ModBiomes;
 import com.dna.everythingisbad.utils.RandomUtils;
 import com.dna.everythingisbad.world.structures.WorldGenLandMine;
 import com.dna.everythingisbad.world.structures.WorldGenTwinTowers;
-import com.dna.everythingisbad.world.structures.WorldGenVillageCasino;
 import com.dna.everythingisbad.world.structures.WorldGenWoolBlock;
 import net.minecraft.init.Biomes;
 import net.minecraft.init.Blocks;
@@ -46,10 +45,11 @@ public class StructureGenerator implements IWorldGenerator {
                 worldGenWoolBlock.generate(world, random, position);
             }
             if (random.nextFloat() < 0.005f) {
-                WorldGenVillageCasino worldGenVillageCasino = new WorldGenVillageCasino();
-                BlockPos position = new BlockPos(xPos, 1, zPos);
-
-                worldGenVillageCasino.generate(world, random, world.getTopSolidOrLiquidBlock(position));
+                //TODO: Re add this in 0.2.0
+//                WorldGenVillageCasino worldGenVillageCasino = new WorldGenVillageCasino();
+//                BlockPos position = new BlockPos(xPos, 1, zPos);
+//
+//                worldGenVillageCasino.generate(world, random, world.getTopSolidOrLiquidBlock(position));
 
             }
             if(RandomUtils.withinChance(200)){
