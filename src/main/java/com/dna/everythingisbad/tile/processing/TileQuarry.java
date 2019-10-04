69879e916450959b3c2e7d04b0870ec969b31a09
DeaSTL <jhartway99@gmail.com>
2019-10-04 15:12:55 -0400

    Quarry is now more balanced.

--- src/main/java/com/dna/everythingisbad/tile/processing/TileQuarry.java
+++ src/main/java/com/dna/everythingisbad/tile/processing/TileQuarry.java
@@ -1,17 +1,23 @@
 package com.dna.everythingisbad.tile.processing;
 
 import com.dna.everythingisbad.tile.TileMachineBase;
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 
 public class TileQuarry extends TileMachineBase {
-    BlockPos drillDifferential = new BlockPos(0,0,0);
-    boolean drillWorking = false;
+    BlockPos relativePosition = new BlockPos(0,0,0);
+    Block currentBlock;
+    Block[] nonMinableBlocks = new Block[]{
+        Blocks.AIR,
+        Blocks.BEDROCK
+    };
+    boolean drillWorking = true;
 
     public TileQuarry() {
         super("quarry");
-        setFinishedProgress(100);
+        setFinishedProgress(10);
     }
 
 
@@ -25,32 +31,61 @@ public class TileQuarry extends TileMachineBase {
         return energyHandler.getEnergyStored() > 100;
     }
 
+
     @Override
     public void insertOutput() {
-        BlockPos currentPos = pos.subtract(drillDifferential).down();
-        for(int x = 0;x<16;x++){
-            for(int z = 0;z<16;z++){
-                IBlockState currentBlock = world.getBlockState(currentPos.add(x,0,z));
+        if(drillWorking) {
 
-                if(currentBlock.getBlock() != Blocks.AIR) {
-                    currentBlock.getBlock().dropBlockAsItem(world, pos.up(), currentBlock, 0);
-                }
-                if(currentBlock.getBlock() != Blocks.BEDROCK) {
-                    world.setBlockToAir(currentPos.add(x, 0, z));
-                }
+            if(pos.getY() - relativePosition.getY() >= 1){
+                relativePosition = relativePosition.add(0,1,0);
+            }else{
+                relativePosition = relativePosition.subtract(new BlockPos(0,relativePosition.getY(),0));
+                relativePosition = relativePosition.add(1,0,0);
             }
+            if(relativePosition.getX() >= 64){
+                relativePosition = relativePosition.subtract(new BlockPos(relativePosition.getX(),0,0));
+                relativePosition = relativePosition.add(0,0,1);
+            }
+            if(relativePosition.getZ() >= 64){
+                drillWorking = false;
+            }
+            BlockPos currentPos = pos.subtract(relativePosition).down().subtract(new BlockPos(1,0,1));;
+            IBlockState currentBlock = world.getBlockState(currentPos);
+            if(canBeMined(currentBlock.getBlock())) {
+                currentBlock.getBlock().dropBlockAsItem(world, pos.up(), currentBlock, 0);
+                world.setBlockToAir(currentPos);
+                energyHandler.reduceEnergy((int)currentBlock.getBlockHardness(world,currentPos),false);
+            }
+
         }
 
     }
-
+    public boolean canBeMined(Block currentBlock){
+        for(Block block:nonMinableBlocks){
+            if(currentBlock == block){
+                return false;
+            }
+        }
+        return true;
+    }
     @Override
     public void reduceInput() {
-        energyHandler.reduceEnergy(100,true);
-        BlockPos currentPos = pos.subtract(drillDifferential);
-        if(currentPos.getY() > 3){
-            drillDifferential = drillDifferential.add(0,1,0);
-        }else{
-            drillDifferential = new BlockPos(drillDifferential.getX()+16,0,drillDifferential.getZ());
-        }
+
+//            energyHandler.reduceEnergy(100, true);
+//            BlockPos currentPos = pos.subtract(relativePosition).subtract(new BlockPos(17,0,17));
+//            if (currentPos.getY() > 3) {
+//                relativePosition = relativePosition.add(0, 1, 0);
+//            } else {
+//                if (relativePosition.getX() <= 64) {
+//                    relativePosition = new BlockPos(relativePosition.getX() + 16, 0, relativePosition.getZ());
+//                }else{
+//                    relativePosition = new BlockPos(0, 0, relativePosition.getZ()+16);
+//                }
+//                if(relativePosition.getZ() >= 64){
+//                    drillWorking = false;
+//                }
+//
+//            }
+//        }
     }
 }
