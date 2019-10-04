ca20ef6dcfed37f476ae51734a8217b66e7bb715
anakrusis <howtofamitracker@gmail.com>
2019-10-04 16:15:43 -0400

    Added adrenaline potion recipe, and Happy Trees don't spawn in superflat
    Signed-off-by: anakrusis <howtofamitracker@gmail.com>

--- src/main/java/com/dna/everythingisbad/world/trees/WorldGenHappyTreeGenerator.java
+++ src/main/java/com/dna/everythingisbad/world/trees/WorldGenHappyTreeGenerator.java
@@ -2,8 +2,11 @@ package com.dna.everythingisbad.world.trees;
 
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldType;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.ChunkGeneratorFlat;
 import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraft.world.gen.feature.WorldGenBirchTree;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
 import java.util.Random;
@@ -17,13 +20,14 @@ public class WorldGenHappyTreeGenerator implements IWorldGenerator {
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
         if(random.nextFloat() < 0.1) {
-            //needs to be within the chunk bounds
-            int xPos = chunkX * 16 + 8;
-            int zPos = chunkZ * 16 + 8;
-            WorldGenTreeHappy tree = new WorldGenTreeHappy();
-            BlockPos top = world.getTopSolidOrLiquidBlock(new BlockPos(xPos, 1, zPos));
-            tree.generate(world, random, new BlockPos(xPos, top.getY(), zPos));
-
+            if (world.getWorldType() != WorldType.FLAT){
+                //needs to be within the chunk bounds
+                int xPos = chunkX * 16 + 8;
+                int zPos = chunkZ * 16 + 8;
+                WorldGenTreeHappy tree = new WorldGenTreeHappy();
+                BlockPos top = world.getTopSolidOrLiquidBlock(new BlockPos(xPos, 1, zPos));
+                tree.generate(world, random, new BlockPos(xPos, top.getY(), zPos));
+            }
         }
     }
 }
