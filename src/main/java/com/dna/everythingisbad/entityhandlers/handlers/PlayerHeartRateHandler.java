33b6a3df4b4ff33c384508767feafd15d7d0037a
anakrusis <howtofamitracker@gmail.com>
2019-10-17 00:52:03 -0400

    Heart rate changes adaptively if you walk, run, swim or stand still
    Signed-off-by: anakrusis <howtofamitracker@gmail.com>

--- src/main/java/com/dna/everythingisbad/entityhandlers/handlers/PlayerHeartRateHandler.java
+++ src/main/java/com/dna/everythingisbad/entityhandlers/handlers/PlayerHeartRateHandler.java
@@ -23,7 +23,7 @@ public class PlayerHeartRateHandler extends PlayerHandlerBase {
         }
         // Ditto with the target heart rate
         if (entitydata.getFloat("target_heart_rate") == 0f){
-            entitydata.setFloat("target_heart_rate",140f);
+            entitydata.setFloat("target_heart_rate",70f);
             player.writeEntityToNBT(entitydata);
         }
     }
@@ -34,6 +34,7 @@ public class PlayerHeartRateHandler extends PlayerHandlerBase {
         // Resets the player's heart rate upon respawn back to 70
         NBTTagCompound entitydata = player.getEntityData();
         entitydata.setFloat("heart_rate",70f);
+        entitydata.setFloat("target_heart_rate",70f);
         player.writeEntityToNBT(entitydata);
     }
 
@@ -52,13 +53,35 @@ public class PlayerHeartRateHandler extends PlayerHandlerBase {
     @Override
     public void playerTick(EntityPlayer player) {
         super.playerTick(player);
+
+        // Updating the target heart rate once every two seconds
+        if (player.ticksExisted % 40 == 39){
+            float target;
+            // If the player is sprinting
+            if (player.isSprinting()) {
+                target = 150f;
+            // If the player is in water, swimming
+            }else if (player.isInWater()){
+                target = 140f;
+            // If the player is standing still and not jumping, not falling, not walking
+            }else if (player.motionX < 0.0001d && player.motionZ < 0.0001d && player.motionY < 0.0001d){
+                target = 70f;
+            // If the player is walking or jumping
+            }else{
+                target = 105f;
+            }
+            NBTTagCompound entitydata = player.getEntityData();
+            entitydata.setFloat("target_heart_rate",target);
+            player.writeEntityToNBT(entitydata);
+        }
+
         // Updating the heart rate once per second
         if (player.ticksExisted % 20 == 19){
             float target = player.getEntityData().getFloat("target_heart_rate");
             float oldrate = player.getEntityData().getFloat("heart_rate");
             float newrate;
             float dist = target-oldrate;
-            newrate = (float)( (float)oldrate + ((1f/45f) * (float)dist));
+            newrate = (float)( (float)oldrate + ((1f/40f) * (float)dist));
 
             NBTTagCompound entitydata = player.getEntityData();
             entitydata.setFloat("heart_rate",newrate);
