b0f7f6a0668cffe82549295fecc74eb77a5e9373
anakrusis <howtofamitracker@gmail.com>
2019-10-25 19:10:23 -0400

    Added baby soup
    Signed-off-by: anakrusis <howtofamitracker@gmail.com>

---
+++ src/main/java/com/dna/everythingisbad/item/food/ItemBabySoup.java
@@ -0,0 +1,15 @@
+package com.dna.everythingisbad.item.food;
+
+import com.dna.everythingisbad.creativetab.CreativeTab;
+import com.dna.everythingisbad.init.ModItems;
+import com.dna.everythingisbad.utils.CommonUtils;
+
+public class ItemBabySoup extends ItemFoodBase {
+    public ItemBabySoup(String name) {
+        super(4, 4.5f, false);
+        setRegistryName(name);
+        setUnlocalizedName(CommonUtils.createUnlocalizedName(name));
+        this.setCreativeTab(CreativeTab.EVERYTHING_BAD_TAB);
+        ModItems.ITEMS.add(this);
+    }
+}
