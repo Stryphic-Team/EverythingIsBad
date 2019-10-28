8b61308d5156f3ff62750d09ce8f3a352e9cff44
DeaSTL <jhartway99@gmail.com>
2019-10-28 16:22:27 -0400

    Added stupid facility structure files

--- src/main/java/com/dna/everythingisbad/utils/helpers/FormatHelper.java
+++ src/main/java/com/dna/everythingisbad/utils/helpers/FormatHelper.java
@@ -6,6 +6,6 @@ public class FormatHelper {
         int exp = (int) (Math.log(count) / Math.log(1000));
         return String.format("%.1f %c",
                 count / Math.pow(1000, exp),
-                "kMGTPE".charAt(exp-1));
+                "KMBTQE".charAt(exp-1));
     }
 }
